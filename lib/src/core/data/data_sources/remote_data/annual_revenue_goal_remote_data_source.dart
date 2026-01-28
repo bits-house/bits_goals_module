a8797103f1fbd4d6d8234e6e5694784c2a4b0dd0
Matheus Grossi <hi@matheusgrossi.dev>
2026-01-28 20:31:25 -0300

    feat(data): update createMonthlyGoalsForYear to include ActionLog and refine error handling

--- lib/src/core/data/data_sources/remote_data/annual_revenue_goal_remote_data_source.dart
+++ lib/src/core/data/data_sources/remote_data/annual_revenue_goal_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:bits_goals_module/src/core/data/exceptions/server_exception_reason.dart';
+import 'package:bits_goals_module/src/core/data/models/action_log/action_log_model.dart';
 import 'package:bits_goals_module/src/core/data/models/monthly_revenue_goal_remote_model.dart';
 
 /// Remote data source for managing annual revenue goals.
@@ -7,17 +9,19 @@ abstract class AnnualRevenueGoalRemoteDataSource {
   /// Persists a list of [MonthlyRevenueGoalRemoteModel].
   ///
   /// Throws:
-  /// - [ServerExceptionType.conflict] if monthly goals for the specified year already exist
-  /// - [ServerExceptionType.permissionDenied] if permission is denied
-  /// - [ServerException] for other server errors
+  /// - [ServerExceptionReason.conflict] if monthly goals for the specified year already exist
+  /// - [ServerExceptionReason.permissionDenied] if permission is denied
+  /// - [ServerExceptionReason] for other server errors
   ///
   /// Rules (for the implementer):
-  /// - This operation MUST be atomic: either all monthly goals are persisted,
+  /// - This operation MUST be atomic: either all monthly goals and logs are persisted,
   ///     or nothing is persisted at all.
   /// - MUST validate if monthly goals for the specified year already exist atomically,
   ///     with the write operation (it cannot be a separate read operation).
+  /// - MUST write logs using the [ActionLogModel] provided.
   Future<void> createMonthlyGoalsForYear({
     required int year,
     required List<MonthlyRevenueGoalRemoteModel> goals,
+    required ActionLogModel log,
   });
 }
