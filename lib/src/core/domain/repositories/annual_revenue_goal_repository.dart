8ee72473248ab33568a6a0acbf63edcc2f19ec09
Matheus Grossi <hi@matheusgrossi.dev>
2026-02-13 20:44:54 -0300

    refactor(repository): update create method to align with ADR-0015 exception handling rules

--- lib/src/core/domain/repositories/annual_revenue_goal_repository.dart
+++ lib/src/core/domain/repositories/annual_revenue_goal_repository.dart
@@ -22,8 +22,6 @@ abstract class AnnualRevenueGoalRepository {
   ///     If an annual revenue goal for the specified year already exists,
   ///     a Failure MUST be thrown.
   /// - MUST write logs using [ActionLog] provided.
-  // TODO: Use Either for return type to properly model failures instead of throwing them,
-  //  to align with ADR-0015.
   Future<AnnualRevenueGoal> create({
     required AnnualRevenueGoal goal,
     required ActionLog log,
