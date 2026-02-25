180974b836b07a8fc070c1b8633ebaf69efad912
Matheus Grossi <hi@matheusgrossi.dev>
2026-02-25 19:28:18 -0300

    feat(presentation): enhance CreateAnnualRevenueGoalButton to accept unavailable years

--- lib/src/features/goals_management/presentation/create_annual_revenue_goal/create_annual_revenue_goal_button.dart
+++ lib/src/features/goals_management/presentation/create_annual_revenue_goal/create_annual_revenue_goal_button.dart
@@ -1,5 +1,6 @@
 import 'package:bits_goals_module/src/core/application/ports/access_control_service.dart';
 import 'package:bits_goals_module/src/core/domain/policies/goals_module_permission.dart';
+import 'package:bits_goals_module/src/core/domain/value_objects/year.dart';
 import 'package:bits_goals_module/src/features/goals_management/presentation/create_annual_revenue_goal/create_annual_revenue_goal_dialog/create_annual_revenue_goal_dialog.dart';
 import 'package:bits_goals_module/src/infra/adapters/access_control_service_impl.dart';
 import 'package:bits_goals_module/src/infra/config/goals_module_config.dart';
@@ -10,23 +11,24 @@ enum _CreateAnnualRevenueGoalButtonType {
 }
 
 class CreateAnnualRevenueGoalButton extends StatelessWidget {
-  static const IconData _defaultIconData = Icons.add;
-
   final GoalsModuleConfig config;
-  final IconData iconData;
+  final List<Year>? unavailableYears;
+  final IconData? iconData;
   final Color? foregroundColor;
   final Color? backgroundColor;
   final _CreateAnnualRevenueGoalButtonType _type;
 
   factory CreateAnnualRevenueGoalButton.fabLarge({
     required GoalsModuleConfig config,
+    List<Year>? unavailableYears,
     IconData? iconData,
     Color? foregroundColor,
     Color? backgroundColor,
   }) {
     return CreateAnnualRevenueGoalButton._internal(
       config: config,
-      iconData: iconData ?? _defaultIconData,
+      unavailableYears: unavailableYears,
+      iconData: iconData,
       foregroundColor: foregroundColor,
       backgroundColor: backgroundColor,
       type: _CreateAnnualRevenueGoalButtonType.fabLarge,
@@ -35,7 +37,8 @@ class CreateAnnualRevenueGoalButton extends StatelessWidget {
 
   const CreateAnnualRevenueGoalButton._internal({
     required this.config,
-    this.iconData = _defaultIconData,
+    this.unavailableYears,
+    this.iconData,
     this.foregroundColor,
     this.backgroundColor,
     required _CreateAnnualRevenueGoalButtonType type,
@@ -47,7 +50,9 @@ class CreateAnnualRevenueGoalButton extends StatelessWidget {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return const CreateAnnualRevenueGoalDialog();
+        return CreateAnnualRevenueGoalDialog(
+          unavailableYears: unavailableYears ?? [],
+        );
       },
     );
   }
@@ -57,6 +62,7 @@ class CreateAnnualRevenueGoalButton extends StatelessWidget {
     const requiredPermission = GoalsModulePermission.createAnnualRevenueGoals;
     final AccessControlService accessControl = AccessControlServiceImpl(config);
     final hasPermission = accessControl.hasPermission(requiredPermission);
+    final buttonIconData = iconData ?? Icons.add;
     return hasPermission
         ? switch (_type) {
             _CreateAnnualRevenueGoalButtonType.fabLarge =>
@@ -66,7 +72,7 @@ class CreateAnnualRevenueGoalButton extends StatelessWidget {
                 ),
                 backgroundColor: backgroundColor,
                 foregroundColor: foregroundColor,
-                child: Icon(iconData),
+                child: Icon(buttonIconData),
               ),
           }
         : const SizedBox.shrink();
