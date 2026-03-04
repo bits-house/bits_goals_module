19fa0c69f6c00d54ae7decd9099dc50a8edba648
Matheus Grossi <hi@matheusgrossi.dev>
2026-03-04 10:11:31 -0300

    feat(presentation): implement permission-based visibility for CreateAnnualRevenueGoalButton

---
+++ lib/src/core/presentation/widgets/show_if_permitted.dart
@@ -0,0 +1,24 @@
+import 'package:bits_goals_module/bits_goals_module.dart';
+import 'package:bits_goals_module/src/core/application/ports/access_control_service.dart';
+import 'package:bits_goals_module/src/core/presentation/state_management/reactivity_flutter/app_provider.dart';
+import 'package:bits_goals_module/src/infra/adapters/access_control_service_impl.dart';
+import 'package:flutter/material.dart';
+
+class ShowIfPermitted extends StatelessWidget {
+  const ShowIfPermitted({
+    super.key,
+    required this.child,
+    required this.requiredPermission,
+  });
+
+  final Widget child;
+  final GoalsModulePermission requiredPermission;
+
+  @override
+  Widget build(BuildContext context) {
+    final config = context.get<GoalsModuleConfig>();
+    final AccessControlService accessControl = AccessControlServiceImpl(config);
+    final hasPermission = accessControl.hasPermission(requiredPermission);
+    return hasPermission ? child : const SizedBox.shrink();
+  }
+}
--- lib/src/features/goals_management/presentation/create_annual_revenue_goal/create_annual_revenue_goal_button.dart
+++ lib/src/features/goals_management/presentation/create_annual_revenue_goal/create_annual_revenue_goal_button.dart
@@ -1,9 +1,6 @@
-import 'package:bits_goals_module/src/core/application/ports/access_control_service.dart';
 import 'package:bits_goals_module/src/core/domain/policies/goals_module_permission.dart';
-import 'package:bits_goals_module/src/core/presentation/state_management/reactivity_flutter/app_provider.dart';
+import 'package:bits_goals_module/src/core/presentation/widgets/show_if_permitted.dart';
 import 'package:bits_goals_module/src/features/goals_management/presentation/create_annual_revenue_goal/create_dialog/create_annual_revenue_goal_dialog.dart';
-import 'package:bits_goals_module/src/infra/adapters/access_control_service_impl.dart';
-import 'package:bits_goals_module/src/infra/config/goals_module_config.dart';
 import 'package:flutter/material.dart';
 
 enum _CreateAnnualRevenueGoalButtonType {
@@ -11,10 +8,27 @@ enum _CreateAnnualRevenueGoalButtonType {
 }
 
 class CreateAnnualRevenueGoalButton extends StatelessWidget {
+  /// A list of years that should be unavailable for selection in the dialog
+  /// when creating a new annual revenue goal. This is used to prevent creating goals
+  /// for years that already have goals or are otherwise restricted.
+  ///
+  /// If not provided, all years will be available for selection and the use case
+  /// will return a Failure if the user tries to create a goal for a year that
+  /// already has one.
   final List<int>? unavailableYears;
+
+  /// The icon to display inside the FAB. If not provided,
+  /// it will default to the "add" icon.
   final IconData? iconData;
+
+  /// The color of the icon and text inside the FAB. If not provided, it will use
+  /// the default FAB foreground color from the current theme.
   final Color? foregroundColor;
+
+  /// The background color of the FAB. If not provided, it will use the default FAB
+  /// background color from the current theme.
   final Color? backgroundColor;
+
   final _CreateAnnualRevenueGoalButtonType _type;
 
   factory CreateAnnualRevenueGoalButton.fabLarge({
@@ -55,24 +69,24 @@ class CreateAnnualRevenueGoalButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Create permission Widget Wrapper
-    const requiredPermission = GoalsModulePermission.createAnnualRevenueGoals;
-    final config = context.get<GoalsModuleConfig>();
-    final AccessControlService accessControl = AccessControlServiceImpl(config);
-    final hasPermission = accessControl.hasPermission(requiredPermission);
+    // If no custom icon is provided, default to the "add" icon.
     final buttonIconData = iconData ?? Icons.add;
-    return hasPermission
-        ? switch (_type) {
-            _CreateAnnualRevenueGoalButtonType.fabLarge =>
-              FloatingActionButton.large(
-                onPressed: () => _onPressed(
-                  context: context,
-                ),
-                backgroundColor: backgroundColor,
-                foregroundColor: foregroundColor,
-                child: Icon(buttonIconData),
-              ),
-          }
-        : const SizedBox.shrink();
+
+    return ShowIfPermitted(
+      // Show the button only if the user has the required permission to
+      // create annual revenue goals.
+      requiredPermission: GoalsModulePermission.createAnnualRevenueGoals,
+
+      // Depending on the button type, render the appropriate FAB variant.
+      child: switch (_type) {
+        _CreateAnnualRevenueGoalButtonType.fabLarge =>
+          FloatingActionButton.large(
+            onPressed: () => _onPressed(context: context),
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor,
+            child: Icon(buttonIconData),
+          ),
+      },
+    );
   }
 }
