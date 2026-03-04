ee33f37ee80024d4505e1fa7d2dda43d336f7b27
Matheus Grossi <hi@matheusgrossi.dev>
2026-03-03 23:51:02 -0300

    feat(domain): add currencyMismatch reason to InvalidMoneyReason enum

--- lib/src/core/domain/failures/value_objects/money/invalid_money_reason.dart
+++ lib/src/core/domain/failures/value_objects/money/invalid_money_reason.dart
@@ -4,4 +4,8 @@ enum InvalidMoneyReason {
 
   /// The Money.value is negative when attempting to split.
   splitNegativeCents,
+
+  /// The currencies of the Money instances do not match when performing
+  /// operations like addition or subtraction.
+  currencyMismatch,
 }
